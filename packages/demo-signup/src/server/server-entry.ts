10898ec2636eb00a51ac07ee884e64839dccceb4
Tao Wen <taowen@chengfayun.com>
2022-04-14 15:53:21 +0800

    hand write routing

--- packages/demo-signup/src/server/server-entry.ts
+++ packages/demo-signup/src/server/server-entry.ts
@@ -1,23 +1,17 @@
-/// <reference types="vite-plugin-pages/client" />
 /// <reference types="incremental-html/jsx" />
 import { Request, Response } from 'express';
 import { jsxToHtml } from 'incremental-html';
-import { createMemoryHistory, createRouter } from 'vue-router';
-import routes from '~pages';
+import * as indexPage from './pages/[...all]'
+import * as newsletter from './pages/newsletter'
 
 export default async function (req: Request, resp: Response) {
-    const router = createRouter({
-        history: createMemoryHistory(),
-        routes
-    });
-    const route = router.resolve(req.originalUrl);
-    const comp = route.matched[0]?.components?.default;
-    if (!comp) {
-        resp.status(404).end('page not found: ' + req.originalUrl);
-        return '';
+    let matched = undefined as any;
+    if (req.originalUrl.startsWith('/newsletter')) {
+        matched = newsletter;
+    } else {
+        matched = indexPage;
     }
-    const page = await comp();
-    const methodHandler = page[req.method];
+    const methodHandler = matched[req.method];
     if (!methodHandler) {
         resp.status(404).end('missing handler for method: ' + req.method);
         return '';
--- packages/demo-signup/vite.config.ts
+++ packages/demo-signup/vite.config.ts
@@ -1,11 +1,6 @@
-import Pages from 'vite-plugin-pages';
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     plugins: [
-      Pages({
-          dirs: 'src/server/pages',
-          extensions: ['tsx']
-      }),
     ],
 })
