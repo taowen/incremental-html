091f2f0d1229b2cc5dc54e0b867f536def226a1a
Tao Wen <taowen@chengfayun.com>
2022-04-18 09:07:49 +0800

    use typescript project reference as monorepo build

--- packages/submit-form/src/submitForm.ts
+++ packages/submit-form/src/submitForm.ts
@@ -1,2 +1,8 @@
-export function submitForm() {
+export async function submitForm(form: HTMLFormElement) {
+    const formData = new FormData(form);
+    const resp = await fetch(form.action, {
+        method: form.method,
+        body: formData
+    })
+    console.log(await resp.json());
 }
