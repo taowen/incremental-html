3ea63d0235192ec0eaddc5426b667a04530b2637
Tao Wen <taowen@chengfayun.com>
2022-04-26 11:38:21 +0800

    make TabPanel working

--- packages/headlessui/src/Tab.ts
+++ packages/headlessui/src/Tab.ts
@@ -2,6 +2,7 @@ import { featureOf, reactive } from '@incremental-html/reactivity';
 
 export class TabGroup {
     private tabs: Tab[] = [];
+    private panels: TabPanel[] = [];
     public store: { selectedIndex: number };
     constructor(props: { element: Element, selectedIndex?: number }) {
         this.store = reactive({ selectedIndex: props.selectedIndex || 0 });
@@ -10,6 +11,12 @@ export class TabGroup {
     public registerTab(tab: Tab) {
         const index = this.tabs.length;
         this.tabs.push(tab);
+        return index;
+    }
+
+    public registerPanel(panel: TabPanel) {
+        const index = this.panels.length;
+        this.panels.push(panel);
         return index;
     }
 }
@@ -21,7 +28,30 @@ export class TabList {
     }
 }
 
+export class TabPanel {
+    private index: number;
+    private tabGroup: TabGroup;
+    constructor(props: { element: Element, vertical?: boolean }) {
+        props.element.setAttribute('role', 'tabpanel');
+        props.element.setAttribute('bind:style.display', "this.tabPanel.selected ? 'block' : 'none'");
+        const tabGroup = featureOf(props.element, TabGroup);
+        if (!tabGroup) {
+            throw new Error('use:tab-panel requires use:tab-group in its ancestor element');
+        }
+        this.tabGroup = tabGroup;
+        this.index = tabGroup.registerPanel(this);
+    }
+
+    public get selected() {
+        return this.tabGroup.store.selectedIndex === this.index;
+    }
+}
+
 export class Tab {
+    public static Group = TabGroup;
+    public static List = TabList;
+    public static Panel = TabPanel;
+
     private index: number;
     private tabGroup: TabGroup;
     constructor(private props: { element: Element }) {
@@ -29,7 +59,7 @@ export class Tab {
         props.element.addEventListener('click', this.onClick.bind(this));
         const tabGroup = featureOf(props.element, TabGroup);
         if (!tabGroup) {
-            throw new Error('use:tab requires use:tabGroup in its ancestor element');
+            throw new Error('use:tab requires use:tab-group in its ancestor element');
         }
         this.tabGroup = tabGroup;
         this.index = tabGroup.registerTab(this);
