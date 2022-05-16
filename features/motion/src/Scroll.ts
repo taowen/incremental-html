aa4650546a5fe431f7d2296f38f3081c15448e1b
Tao Wen <taowen@chengfayun.com>
2022-05-16 16:10:04 +0800

    add Scroll.Pullable

--- features/motion/src/Scroll.ts
+++ features/motion/src/Scroll.ts
@@ -1,11 +1,44 @@
 import { animate, MotionProps, motionValue } from "@incremental-html/framer-motion";
-import { Feature } from "@incremental-html/reactivity";
+import { morphChildNodes } from "@incremental-html/morph";
+import { Feature, queryFeature, Ref, ref } from "@incremental-html/reactivity";
+import { render } from "@incremental-html/template";
 import { Motion } from "./Motion";
 
-export class Scroll extends Feature<MotionProps> {
+class ScrollPullable extends Feature<{ handle?: () => Promise<void>; }> {
+    private get scroll() {
+        const scroll = queryFeature(this.element, Scroll);
+        if (!scroll) {
+            throw new Error('can not use Scroll.Pullable without Scroll');
+        }
+        return scroll;
+    }
+    private template = this.create(() => {
+        const template = document.createElement('template');
+        let child = this.element.firstChild;
+        while (child) {
+            template.content.appendChild(child.cloneNode(true));
+            child = child.nextSibling;
+        }
+        return template;
+    })
+    private _ = this.effect(() => {
+        const nodes = render(this.template, { status: this.scroll.status });
+        morphChildNodes(this.element, nodes);
+    })
+}
+
+export class Scroll extends Feature<MotionProps & { onPullDown?: () => Promise<void>; }> {
+    public static Pullable = ScrollPullable;
     private dragStart: number;
+    private threshold: number;
+    private refStatus: Ref<'default' | 'will-handle' | 'handling'> = ref('default')
+    public dragY = motionValue(0);
+    public get status() {
+        return this.refStatus.value;
+    }
     private onPanSessionStart: MotionProps['onPanSessionStart'] = () => {
         this.dragStart = this.element.scrollTop;
+        this.threshold = this.pullable ? this.pullable.element.getBoundingClientRect().height : 0;
     }
     private onPanStart: MotionProps['onPanStart'] = (e, { offset }) => {
         this.applyOffset(offset.y);
@@ -13,14 +46,28 @@ export class Scroll extends Feature<MotionProps> {
     private onPan: MotionProps['onPan'] = (e, { offset }) => {
         this.applyOffset(offset.y);
     }
-    private onPanEnd: MotionProps['onPanEnd'] = (e, { offset }) => {
-        animate(this.innerY, 0);
+    private onPanEnd: MotionProps['onPanEnd'] = async (e, { offset }) => {
+        if (this.pullable?.props?.handle && this.status === 'will-handle') {
+            animate(this.dragY, this.threshold);
+            try {
+                this.refStatus.value = 'handling';
+                await this.pullable.props.handle();
+            } finally {
+                this.refStatus.value = 'default';
+                animate(this.dragY, 0);
+            }
+        } else {
+            this.refStatus.value = 'default';
+            animate(this.dragY, 0);
+        }
     }
     private applyOffset(offsetY: number) {
         const pos = this.dragStart - offsetY;
-        if (pos < 0) {
-            this.innerY.set(-pos * 0.4);
+        if (this.pullable && pos < 0) {
             this.element.scrollTop = 0;
+            const dragY = -pos * 0.4;
+            this.dragY.set(dragY);
+            this.refStatus.value = dragY > this.threshold ? 'will-handle' : 'default'
         } else {
             this.element.scrollTop = pos;
         }
@@ -41,15 +88,17 @@ export class Scroll extends Feature<MotionProps> {
         })
         return new Motion(this.element, () => this.mergedProps);
     })
-    private innerY = motionValue(0);
     private get inner() {
         return this.element.firstElementChild as HTMLElement;
+    }
+    private get pullable() {
+        return queryFeature(this.inner.firstElementChild, ScrollPullable);
     }
     private innerMotion: Motion = this.create(() => {
         Object.assign(this.inner.style, {
             userSelect: 'none',
             touchAction: 'pan-x',
         })
-        return new Motion(this.inner, () => ({ style: { y: this.innerY } }));
+        return new Motion(this.inner, () => ({ style: { y: this.dragY } }));
     })
 }
