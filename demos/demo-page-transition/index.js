f64786660329102d79ba4f92efda81fca3beb40c
Tao Wen <taowen@chengfayun.com>
2022-05-27 17:01:23 +0800

    extract out server packages

--- demos/demo-page-transition/index.js
+++ demos/demo-page-transition/index.js
@@ -2,6 +2,9 @@ import * as quicklink from 'quicklink';
 import { startDomObserver, setEvalGlobals } from '@incremental-html/reactivity';
 import { Motion } from '@incremental-html/motion';
 
+// quicklink is optional, it is just to demo Chrome is making MPA faster and faster
+// https://getquick.link/demo/ This page contains some demo sites that use Quicklink to improve navigation
+// https://developer.chrome.com/blog/paint-holding/ Paint Holding - reducing the flash of white on same-origin navigations
 window.addEventListener('load', () => {
   quicklink.listen();
 });
---
+++ server/README.md
@@ -0,0 +1,3 @@
+# Optional packages for node.js
+
+If you use PHP/Go/Python... in the server side, ignore this directory
