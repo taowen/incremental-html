147dd3567bd5ffac871d778529cb92189c86ac33
Tao Wen <taowen@chengfayun.com>
2022-05-27 11:54:31 +0800

    use Reloader to replace

--- demos/demo-gallery/client/client-entry.ts
+++ demos/demo-gallery/client/client-entry.ts
@@ -1,6 +1,5 @@
-import { List } from '@incremental-html/headlessui';
-import { navigator } from '@incremental-html/navigator';
+import { List, Reloader } from '@incremental-html/headlessui';
 import { setEvalGlobals, startDomObserver, closestFeature } from '@incremental-html/reactivity';
 
-setEvalGlobals({ List, navigator, closestFeature })
+setEvalGlobals({ List, Reloader, closestFeature })
 startDomObserver();
--- demos/demo-gallery/server/server.tsx
+++ demos/demo-gallery/server/server.tsx
@@ -71,7 +71,7 @@ function GalleryImage({ imageId, reloadUrl }: { imageId: number, reloadUrl: stri
                     await fetch('/unfav', { body: JSON.stringify({ imageId: Number(this.dataset.imageId) }), method: 'POST', 
                         headers: { 'Content-Type': 'application/json' } });
                     $closestFeature(this, $List.Reloader).reload();
-                    $navigator.reload();">
+                    document.body.reloader.reload();">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
@@ -82,7 +82,7 @@ function GalleryImage({ imageId, reloadUrl }: { imageId: number, reloadUrl: stri
                     await fetch('/fav', { body: JSON.stringify({ imageId: Number(this.dataset.imageId) }), method: 'POST', 
                         headers: { 'Content-Type': 'application/json' } });
                     $closestFeature(this, $List.Reloader).reload();
-                    $navigator.reload();">
+                    document.body.reloader.reload();">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 fill-tranparent hover:fill-red-200" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                 </svg>
@@ -103,13 +103,13 @@ function Tab({ tab, controls, label }: { tab?: string, controls: string, label: 
         return <li role="presentation">
             <button class="inline-flex items-center pl-4 pr-4 h-full rounded-t-lg border-b-2 border-blue-600"
                 type="button" role="tab" aria-controls={controls} aria-selected="true"
-                on:click={`$navigator.href = '/?tab=${controls}'`}>{label}</button>
+                on:click={`document.body.reloader.reload('/?tab=${controls}')`}>{label}</button>
         </li>
     } else {
         return <li role="presentation">
             <button class="inline-flex items-center pl-4 pr-4 h-full rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
                 type="button" role="tab" aria-controls={controls} aria-selected="false"
-                on:click={`$navigator.href = '/?tab=${controls}'`}>{label}</button>
+                on:click={`document.body.reloader.reload('/?tab=${controls}')`}>{label}</button>
         </li>
     }
 }
