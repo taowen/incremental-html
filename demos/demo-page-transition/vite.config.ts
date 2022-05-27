13df2dd569ecc85391c279d93bcc578ffb2639e9
Tao Wen <taowen@chengfayun.com>
2022-05-27 16:44:06 +0800

    demo mpa without server

--- demos/demo-page-transition/vite.config.ts
+++ demos/demo-page-transition/vite.config.ts
@@ -1,4 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+                page1: path.resolve(__dirname, 'page1.html'),
+                page2: path.resolve(__dirname, 'page2.html'),
+            }
+        },
+    }
 })
