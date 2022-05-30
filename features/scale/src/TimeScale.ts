2fe5d7e2d776b244be5d0e11da5094bf54d5a2e3
Tao Wen <taowen@chengfayun.com>
2022-05-30 11:39:33 +0800

    generate ticks from data

--- features/scale/src/TimeScale.ts
+++ features/scale/src/TimeScale.ts
@@ -1,6 +1,18 @@
 import { Feature } from "@incremental-html/reactivity";
+import { extent } from "d3-array";
 import { scaleTime } from 'd3-scale';
+import { CountableTimeInterval } from 'd3-time';
 
-export class TimeScale extends Feature<{ ticksInterval?: 'month' }> {
-    public readonly scale = scaleTime();
+export class TimeScale extends Feature<{ data: Date[], interval?: CountableTimeInterval }> {
+    public readonly scale = this.create(() => {
+        let scale = scaleTime().domain(extent(this.props.data) as [Date, Date]);
+        if (this.props.interval) {
+            scale = scale.nice(this.props.interval)
+        }
+        return scale;
+    });
+
+    public get ticks() {
+        return this.scale.ticks(this.props.interval!);
+    }
 }
--- features/scale/src/__tests__/TimeScale.spec.ts
+++ features/scale/src/__tests__/TimeScale.spec.ts
@@ -1,7 +1,16 @@
-import { TimeScale } from "../TimeScale"
-import * as dateFns from 'date-fns';
+import { utcMonth } from 'd3-time';
+import { TimeScale } from "../TimeScale";
 
-test('scale', () => {
-    const scale = new TimeScale(document.createElement('svg'), () => ({ ticksInterval: 'month' }));
-    console.log(scale.scale(dateFns.parseISO('2014-02-11T11:30:30')));
+test('generate ticks from data', () => {
+    const scale = new TimeScale(document.createElement('svg'), () => ({
+        interval: utcMonth,
+        data: [
+            new Date('2014-02-11T11:30:30'),
+            new Date('2014-01-11T11:30:30'),
+            new Date('2015-06-11T11:30:30')
+        ],
+    }));
+    expect(scale.ticks.length).toBe(19);
+    expect(scale.ticks[0]).toEqual(new Date('2014-01-01T00:00:00.000Z'));
+    expect(scale.ticks[18]).toEqual(new Date('2015-07-01T00:00:00.000Z'));
 })
